fa632cffcdb0052ae63bf2b9bf34a7186b46628a
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-02-07 11:15:17 +0100

    final design draft

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginDate);
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.setTemplateFormats([
+    "md", "jpg"
+  ]);
   return {
     dir: { input: 'src', output: '_site' }
   };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '768px',
+      'xl': '768px',
+      '2xl': '768px',
+    },
+    colors: {
+      white: "#FFF",
+      black: "#000",
+      background: "#1A202C",
+      gray: colors.coolGray,
+      primary: colors.green
+    },
+    extend: {
+
+    },
   },
   variants: {
     extend: {},
